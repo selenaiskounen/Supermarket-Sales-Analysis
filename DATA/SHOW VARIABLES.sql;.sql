77a21c0c500b0bcd31e1f1e5ce5417a8403b847d
selenaiskounen <selena.iskounen@hotmail.com>
2025-06-26 07:32:04 +0200

    delete the old folder's name

--- DATA/SHOW VARIABLES.sql;.sql
+++
@@ -1,8 +0,0 @@
-SHOW VARIABLES LIKE 'secure_file_priv';
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/supermarket_sales.csv'
-INTO TABLE sales_temp
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
